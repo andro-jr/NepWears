db4b297261335f10d98833d32cfc52a27222f643
prabin <prabin20panta@gmail.com>
2023-10-12 22:34:35 +0545

    hero section added

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// SLick slider settings
+$(document).ready(function () {
+  $(".hero-slide").slick({
+    dots: true,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    dots: true,
+    infinite: true,
+    speed: 4000,
+  });
+});
