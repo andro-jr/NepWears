adbef8d9bf601b02abf2ba1195316c4d6f430373
prabin <prabin20panta@gmail.com>
2023-10-13 23:31:28 +0545

    service and categories section

--- js/script.js
+++ js/script.js
@@ -1,14 +1,31 @@
 "use strict";
 
+const headerTop = document.querySelector(".header-top");
+
 // SLick slider settings
 $(document).ready(function () {
   $(".hero-slide").slick({
     dots: true,
-    arrows: true,
+    arrows: false,
     autoplay: true,
     autoplaySpeed: 1000,
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 4000,
   });
 });
+
+let lastScrollTop = 0;
+window.addEventListener(
+  "scroll",
+  function () {
+    var currentScroll = window.scrollY || window.pageYOffset;
+    if (currentScroll > lastScrollTop) {
+      headerTop.classList.add("header-top-show");
+    } else {
+      headerTop.classList.remove("header-top-show");
+    }
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
+  },
+  false
+);
